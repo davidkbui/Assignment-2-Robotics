ff4063b2c268cfe4e6ccfca2fb24df171235dd6a
david.k.bui-1@student.uts.edu.au <david@getspotto.io>
2021-05-22 17:32:53 +1000

    added sensors

--- Assignment2_Final/Environment.m
+++ Assignment2_Final/Environment.m
@@ -18,11 +18,7 @@ classdef Environment < handle
             surf([-axisL,axisL;-axisL,axisL],[axisL,axisL;axisL,axisL],[0,0;axisL,axisL],'CData',imread('window.jpeg'),'FaceColor','texturemap'); % Concrete floor
             surf([axisL,axisL;axisL,axisL],[-axisL,axisL;-axisL,axisL],[0,0;axisL,axisL],'CData',imread('office_wall_1.jpeg'),'FaceColor','texturemap'); % Concrete floor
             
-            PlaceObject('fence1.ply',[-2 0 0]);
-            PlaceObject('fence1.ply',[2 0 0]);
-            PlaceObject('fence2.ply',[0 1.2 0]);
-            PlaceObject('fence2.ply',[0 -1.2 0]);
-
+            PlaceObject('fence.ply',[0 0 0]);
             hold on
             PlaceObject('table.ply',[0.25,0,0]);
 
@@ -32,8 +28,6 @@ classdef Environment < handle
             %PlaceObject('container_yellow.ply',[1.15,-0.3,0.225]);
             
             PlaceObject('firstaid.ply',[2.5,2.5,0]);
-            PlaceObject('DANGER.ply',[-1.9 1.2 1]);
-            PlaceObject('DANGER.ply',[1.3 1.2 1]);
             
             PlaceObject('teddy.ply', [0.9, 0.3,0.28]);
             %indexed out because these ply files are too huge
