07ad30d9a1082a51510263f8847cc51fff488365
rt-l <64583452+rt-l@users.noreply.github.com>
2021-05-27 21:12:16 +1000

    Update VStest.m

--- Assignment2_Final/VStest.m
+++ Assignment2_Final/VStest.m
@@ -1,24 +1,24 @@
-camList = webcamlist;
-cam = webcam(1);
-preview(cam);
+camList = webcamlist; % Create a list of all the webcams
+cam = webcam(1); % Chooses the first webcam
+preview(cam); % Opens webcamfeed
 pause(5);
-im = snapshot(cam);
-imwrite(im, 'bricktest.jpg');
+im = snapshot(cam); % Takes snapshot of current frame
+imwrite(im, 'bricktest.jpg'); % Writes to jpg file
 pause(5);
-img = iread('bricktest.jpg', 'double', 'gamma', 4);
+img = iread('bricktest.jpg', 'double', 'gamma', 4); % Read the image and adjust the gamma for correct filter
 image(img);
-R = img(:,:,1);
+R = img(:,:,1); % Maxtrix of assigning rgb numerical values
 G = img(:,:,2);
 B = img(:,:,3);
-Y = R + G + B;
+Y = R + G + B; 
 r = R ./ Y;
 g = G ./ Y;
 b = B ./ Y;
-Rbrick = (r > 0.95) & (g < 0.05) & (b < 0.05);
+Rbrick = (r > 0.95) & (g < 0.05) & (b < 0.05); % Defining the bounds at which the colours will show up as either 1 or 0
 Bbrick = (b > 0.8) & (r < 0.2);
-Cr = corner(Rbrick);
+Cr = corner(Rbrick); % Taking verticies from the filtered images
 Cb = corner(Bbrick);
-figure()
+figure() % Plot figures of each of the colour masks and then actual image with overlay
 imshow(Rbrick);
 hold on
 plot(Cr(:,1),Cr(:,2),'r*')
